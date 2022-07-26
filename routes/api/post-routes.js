9e716531ce7d27d5fb6104f8191967e3140e8d73
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-07-26 16:22:59 -0400

    feature complete

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 
@@ -7,6 +7,7 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
+        order: [['created_at', 'DESC']],
         attributes: [
             'id',
             'post_url',
@@ -14,19 +15,22 @@ router.get('/', (req, res) => {
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-        order: [['created_at', 'DESC']],
         include: [
+            // include the Comment model here:
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
             }
         ]
     })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
 });
 
 router.get('/:id', (req, res) => {
